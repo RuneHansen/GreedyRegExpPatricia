6994912f63ed86f2f5e882064ef47be7a0e362af
emiljp <girlswithglassesarecute@gmail.com>
2015-03-12 17:40:37 +0100

    skelet til simpel unit test

--- Implementation/Simple/TestSimpleI.cpp
+++ Implementation/Simple/TestSimpleI.cpp
@@ -21,42 +21,42 @@ std::string* regExComplete(std::string regEx, std::string test_input) {
   std::string** ma;
 
   std::cout << "Parser nu\n";
-  
-  
+
+
   BitC_YY_scan_string(regEx.c_str());
   BitC_YYparse(&regex);
   BitC_YYlex_destroy();
-  
+
   //regex = BitC_RegexSimplify(regex);
-  
+
   std::cout << "Har Parset, generer aNFA\n";
-  
+
   aNFAgen(regex, aNFAfirst, aNFAlast, &language);
-  
+
   std::cout << "Har genereret aNFA, tilføjer tal\n";
-  
+
   matrixSize = addNr(aNFAfirst, 0);
 
   std::cout << "Har fundet tal, " << matrixSize << " finder sprog\n";
   std::cout << "Har fundet sprog " << language << ", laver plads til ma\n";
-  
+
   ma = (std::string**) malloc(matrixSize * matrixSize *
                              language.size() * sizeof(std::string*));
-                             
+
   std::cout << "Har lavet plads, bygger matricer\n";
   for(int i = 0; i < language.size(); i++) {
     //ma[i*matrixSize*matrixSize] = buildMatrix(&aNFAfirst, matrixSize, language.at(i));
     buildMatrix(aNFAfirst, matrixSize, language.at(i), ma + (i*matrixSize*matrixSize));
   }
-  
+
   std::cout << "Matricer bygget, laver S\n";
-  
+
   S = (std::string**) malloc(sizeof(std::string*) * matrixSize);
   for(int i = 0; i < matrixSize; i++) {
     S[i] = new std::string();
     *S[i] = createString(aNFAfirst, i, '\0');
   }
-    
+
   std::cout << "S er skabt\n";
 
   std::cout << "Your matrix\n";
@@ -68,7 +68,7 @@ std::string* regExComplete(std::string regEx, std::string test_input) {
 
   std::cout << "aNFA simulation:\n";
   std::cout << "input = " << test_input << "\n";
-  
+
   // Test input validity
   for (int i = 0; i < test_input.length(); i++) {
     if (language.find(test_input[i]) == std::string::npos) {
@@ -89,17 +89,17 @@ std::string* regExComplete(std::string regEx, std::string test_input) {
 
   // Run simulation
   simulate(S, language, ma, matrixSize, is);
-  
+
   std::string* retS = new std::string();
   *retS = *S[aNFAlast->nr];
-  
-  
-  
 
-  
+
+
+
+
   std::cout << "Freeing Memory\n";
-  
-  
+
+
   for(int i = 0; i < matrixSize; i++) {
     delete S[i];
   }
@@ -109,9 +109,70 @@ std::string* regExComplete(std::string regEx, std::string test_input) {
   std::cout << "aNFA freed\n";
   freeMatrix(ma, matrixSize, language.size());
   std::cout << "Matrices freed\n";
-  
+
   return retS;
 }
+
+int newTest() {
+  std::cout << "RNG test " << (char) 122 << "\n";
+
+  int numTests = 10; //<<<<<===================================== HUSK MIG!!!!!!!!!!!!!!!!!!!!!!!!!
+  std::string** regexs = malloc(numTests * sizeof(std::string*));
+  std::string** inputs = malloc(numTests * sizeof(std::string*));
+  std::string** bitstrings = malloc(numTests * sizeof(std::string*));
+
+  regexs[0] = new std::string("abc");
+  inputs[0] = new std::string("abc");
+  bitstrings[0] = new std::string("");
+
+  regexs[1] = new std::string("ab*c");
+  inputs[1] = new std::string("abc");
+  bitstrings[1] = new std::string("01");
+
+  regexs[2] = new std::string("ab*bc");
+  inputs[2] = new std::string("abc");
+  bitstrings[2] = new std::string("1");
+
+  regexs[3] = new std::string("ab*bc");
+  inputs[3] = new std::string("abbc");
+  bitstrings[3] = new std::string("01");
+
+  regexs[4] = new std::string("ab*bc");
+  inputs[4] = new std::string("abbbbc");
+  bitstrings[4] = new std::string("0001");
+
+  regexs[5] = new std::string("ab+bc");
+  inputs[5] = new std::string("abbc");
+  bitstrings[5] = new std::string("1");
+
+  regexs[6] = new std::string("ab+bc");
+  inputs[6] = new std::string("abbbbc");
+  bitstrings[6] = new std::string("001");
+
+  regexs[7] = new std::string("ab?bc");
+  inputs[7] = new std::string("abbc");
+  bitstrings[7] = new std::string("0");
+
+  regexs[8] = new std::string("ab?bc");
+  inputs[8] = new std::string("abc");
+  bitstrings[8] = new std::string("1");
+
+  regexs[9] = new std::string("ab?c");
+  inputs[9] = new std::string("abc");
+  bitstrings[9] = new std::string("0");
+
+  for (int i = 0; i < numTests; i++) {
+    if (bitstring[i] != *regExComplete(*regexs[i],*inputs[i])) {
+      std::cout << "FAILED! At test number " << i
+       << ", returned " << *regExComplete(*regexs[i],*inputs[i])
+        << ", when " << *bitstring[i] << " was expected.\n";
+        return 0;
+    }
+  }
+  std::cout << "SUCCESS! All tests completed."
+  return 0;
+}
+
 
 int main() {
   std::string* result;
@@ -123,7 +184,7 @@ int main() {
   std::cin.getline(input, 100);
 
   result = regExComplete(regEx, input);
-  
+
   std::cout << "Result: " << *result << std::endl;
 
   return 0;
@@ -151,46 +212,46 @@ int main() {
     test_input = std::string(tmpI);
 
     std::cout << "Parser nu\n";
-    
+
     BitC_YY_scan_string(regEx);
     BitC_YYparse(&regex);
     BitC_YYlex_destroy();
-    
+
     //regex = BitC_RegexSimplify(regex);
-    
+
     std::cout << "aNFAlast ptrs = " << aNFAlast->left << " " << aNFAlast->right << "\n";
-    
+
     std::cout << "Har Parset, generer aNFA\n";
-    
+
     aNFAgen(regex, aNFAfirst, aNFAlast, &language);
-    
+
     std::cout << "Har genereret aNFA, tilføjer tal\n";
-    
+
     matrixSize = addNr(aNFAfirst, 0);
 
     std::cout << "Har fundet tal, " << matrixSize << " finder sprog\n";
-    
+
     //language = findLanguage(aNFAfirst);
-    
+
     std::cout << "Har fundet sprog " << language << ", laver plads til ma\n";
-    
+
     ma = (std::string**) malloc(matrixSize * matrixSize *
                                language.size() * sizeof(std::string*));
-                               
+
     std::cout << "Har lavet plads, bygger matricer\n";
     for(int i = 0; i < language.size(); i++) {
       //ma[i*matrixSize*matrixSize] = buildMatrix(&aNFAfirst, matrixSize, language.at(i));
       buildMatrix(aNFAfirst, matrixSize, language.at(i), ma + (i*matrixSize*matrixSize));
     }
-    
+
     std::cout << "Matricer bygget, laver S\n";
-    
+
     S = (std::string**) malloc(sizeof(std::string*) * matrixSize);
     for(int i = 0; i < matrixSize; i++) {
       S[i] = new std::string();
       *S[i] = createString(aNFAfirst, i, '\0');
     }
-    
+
     std::cout << "S er skabt\n";
 
     std::cout << "Your matrix\n";
@@ -217,13 +278,13 @@ int main() {
     S[1] = "";
     S[2] = "0";
     S[3] = "na";
-    S[4] = "1"; 
+    S[4] = "1";
   } */
 
   /*
   std::cout << "aNFA simulation:\n";
   std::cout << "input = " << test_input << "\n";
-  
+
   // Test input validity
   for (int i = 0; i < test_input.length(); i++) {
     if (language.find(test_input[i]) == std::string::npos) {
@@ -247,11 +308,11 @@ int main() {
   std::cout << "Finishing state:\n"; // After simulation
   std::cout << "Last state: " << aNFAlast->nr << std::endl;
   printPaths(S, matrixSize);
-  
+
 
   // Make sure the window does not close automatically.
   std::cout << "\nEnter one character to finish the program.\n";
   char a;
-  std::cin >> a; 
+  std::cin >> a;
 	return 0;
 } */
